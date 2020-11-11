ee2acf7b8e3d04009134f14d8d56fe4588fd763c
koby <kobybitonu@gmail.com>
2020-11-11 15:37:08 +0200

    creating model, controllers and migtrations

---
+++ database/migrations/2020_11_11_120955_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('periodId');
+            $table->string('userName');
+            $table->string('password');
+            $table->string('fullName');
+            $table->integer('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
